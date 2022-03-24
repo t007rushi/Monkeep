4dcc81aff25b0ce7ef498fb7f6b948dee5c3a7bc
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-03-24 20:46:29 +0530

    added routes for homepage and mockman

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
-import Mockman from "mockman-js"
+import Mockman from "mockman-js";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Home/HomePage";
 
 function App() {
   return (
-    <div className="App">
-    <Mockman />
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/mock" element={<Mockman />}></Route>
+      </Routes>
     </div>
   );
 }
 
 export default App;
-
