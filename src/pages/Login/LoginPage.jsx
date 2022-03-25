37b9c031ad3a9a95f4d8d6a8b3b9619e7a51ba88
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-03-25 21:42:03 +0530

    Authorization added for protected route

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -17,13 +17,15 @@ const LoginPage = () => {
   const navigator = useNavigate();
   return (
     <div className="flex-row center-it">
-      <img src="./assets/login.png" alt="profile" className="form-img-desk" />
       <div className="form-wrap">
-        <form className="flex-col center-it form-block" onSubmit={(e) => {
-              e.preventDefault();
-              logInHandler(logUser, setUser, navigator);
-            }}>
-          <img src="./assets/profile.png" alt="profile" className="form-img" />
+        <form
+          className="flex-col center-it form-block"
+          onSubmit={(e) => {
+            e.preventDefault();
+            logInHandler(logUser, setUser, navigator);
+          }}
+        >
+          <img src="./assets/profile.png" alt="profile" className="form-img img-resp" />
           <h2 className="form-title">LOGIN</h2>
           <label htmlFor="email" className="flex-col form-input-txt">
             UserID/Email
@@ -31,7 +33,9 @@ const LoginPage = () => {
               type="email"
               className="form-input-box"
               placeholder="monkstar@neog.camp"
-              onChange={(e) =>  setLogUser({ ...logUser, email: e.target.value })}
+              onChange={(e) =>
+                setLogUser({ ...logUser, email: e.target.value })
+              }
             />
           </label>
           <label htmlFor="password" className="flex-col form-input-txt">
@@ -52,10 +56,13 @@ const LoginPage = () => {
           <button type="submit" className="btn primary-btn">
             LOGIN
           </button>
-          <button className="btn card-btn" onClick={(e) => {
-            e.preventDefault();
-                logInHandler(guestUser, setUser, navigator);
-              }}>
+          <button
+            className="btn card-btn"
+            onClick={(e) => {
+              e.preventDefault();
+              logInHandler(guestUser, setUser, navigator);
+            }}
+          >
             LOGIN as GUEST
           </button>
           <Link to="/signup" className="newAcc">
