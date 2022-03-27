a38dba79d5cad845b5763218b3bb972da0399beb
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-03-28 00:44:20 +0530

    made sidebar component

--- src/Layout/sidebar/Sidebar.jsx
+++ src/Layout/sidebar/Sidebar.jsx
@@ -1,34 +1,26 @@
 import React from "react";
-import { RiInboxArchiveLine } from "react-icons/ri";
-import { FiTrash2 } from "react-icons/fi";
-import { MdOutlineLabel, MdOutlineNote } from "react-icons/md";
-import { CgProfile } from "react-icons/cg";
 import "./sidebar.css";
+import { useNavigate } from "react-router-dom";
+import sidebarData from "../../data/sidebarData";
 
-const Sidebar = ({sidebar}) => {
+const Sidebar = ({ sidebar }) => {
+  const navigator = useNavigate();
+
   return (
-    <nav className={sidebar?"sidebar active":"sidebar"}>
+    <nav className={sidebar ? "sidebar active" : "sidebar"}>
       <ul className="flex-row flex-col spc-btwn  gap-btwn">
-        <li className="flex-row gap-btwn cur-point ">
-          <MdOutlineNote />
-          <span>Notes</span>
-        </li>
-        <li className="flex-row gap-btwn  cur-point">
-          <MdOutlineLabel />
-          <span>Labels</span>
-        </li>
-        <li className="flex-row gap-btwn cur-point">
-          <RiInboxArchiveLine />
-          <span>Archives</span>
-        </li>
-        <li className="flex-row gap-btwn cur-point">
-          <FiTrash2 />
-          <span>Trash</span>
-        </li>
-        <li className="flex-row gap-btwn cur-point">
-          <CgProfile />
-          <span>Profile</span>
-        </li>
+        {sidebarData.map(({ title, navigate, Icon }) => {
+          return (
+            <li
+              key={title}
+              className="flex-row gap-btwn cur-point sidebar-item "
+              onClick={() => navigator(navigate)}
+            >
+              <Icon />
+              <span>{title}</span>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
