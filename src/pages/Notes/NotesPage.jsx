7f37e75e792f693464b40eb1bd5cbb99b77b938a
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-03-29 16:31:23 +0530

    sidebar updation with fixed icon bar

--- src/pages/Notes/NotesPage.jsx
+++ src/pages/Notes/NotesPage.jsx
@@ -1,15 +1,24 @@
-import React,{ useState} from "react";
+import React, { useState } from "react";
 import Sidebar from "../../Layout/sidebar/Sidebar";
 import Header from "../../Layout/Header/Header";
+import CreateNote from "../../components/CreateNote/CreateNote";
+import Note from "../../components/Note/Note";
+import "./notes.css"
 
 export const NotesPage = () => {
-  const [sidebar,setSidebar] = useState(false);
+  const [sidebar, setSidebar] = useState(true);
 
-  const toggle = () => setSidebar(!sidebar)
+  const toggle = () => setSidebar(!sidebar);
   return (
     <div className="notes">
-      <Header toggle={toggle}/>
-      <Sidebar sidebar ={sidebar}/>
+      <Header toggle={toggle} />
+      <div className="main-content">
+      <Sidebar sidebar={sidebar} />
+      <div className={sidebar?"note-content note-hide":"note-content "}>
+      <CreateNote />
+      <Note />
+      </div>
+      </div>
     </div>
   );
 };
