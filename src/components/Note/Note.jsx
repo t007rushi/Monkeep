7ed38c4c13295aece42415345f1c572a07da66ed
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-03-30 11:36:00 +0530

    Note component added to show notes

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { useNotes } from "../../context/notes-context";
+import "./note.css";
+import { FiTrash2 } from "react-icons/fi";
 
 export const Note = () => {
-  return <div>
-    Note
-  </div>;
+  const { note, removeFromnote } = useNotes();
+  return (
+    <div className="note-collection">
+      {note.map((item) => {
+        return (
+          <div className="note" key={item._id}>
+            {item.NoteContent.title}
+            <div
+              className="note-descr"
+              dangerouslySetInnerHTML={{ __html: item.NoteContent.description }}
+            />
+            < FiTrash2 onClick={() => removeFromnote(item._id)}/>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
-
