20e9be709f78a6413e1c951e77ad7da441aad36c
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-03-30 11:38:27 +0530

    style : sidebar expand styling

--- src/pages/Notes/NotesPage.jsx
+++ src/pages/Notes/NotesPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import {Sidebar,Header} from "../../Layout/index.js";
-import {CreateNote,Note} from "../../components/index.js"
-import "./notes.css"
+import { Sidebar, Header } from "../../Layout/index.js";
+import { CreateNote, Note } from "../../components/index.js";
+import "./notes.css";
 
 export const NotesPage = () => {
   const [sidebar, setSidebar] = useState(true);
@@ -11,11 +11,11 @@ export const NotesPage = () => {
     <div className="notes">
       <Header toggle={toggle} />
       <div className="main-content">
-      <Sidebar sidebar={sidebar} />
-      <div className={sidebar?"note-content note-hide":"note-content "}>
-      <CreateNote />
-      <Note />
-      </div>
+        <Sidebar sidebar={sidebar} />
+        <div className={sidebar ? "note-content note-hide" : "note-content "}>
+          <CreateNote />
+          <Note />
+        </div>
       </div>
     </div>
   );
