2c0172a5aa75640b7436409f91eb42f83bbd2b6a
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-03-31 10:40:36 +0530

    toggle theme feat added

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import "./note-header.css";
 import { AiOutlineSearch, AiOutlineMenu } from "react-icons/ai";
 import { MdDarkMode, MdLogout } from "react-icons/md";
+import { useTheme } from "../../context/theme-context";
 
 const Header = ({ toggle }) => {
+  const {toggleTheme} = useTheme();
   return (
     <header className="flex-row header-bar">
       <div className="flex-row spc-btwn header-left">
         <button className="ham-burger" onClick={toggle}>
           <AiOutlineMenu className="header-icon" />
         </button>
-        <h1 className="flex-row web-logo">Monkeep</h1>
+        <h1 className="flex-row web-logo"><i>MON</i>KEEP</h1>
       </div>
       <div className="flex-row header-mid">
         <AiOutlineSearch className="header-icon" />
@@ -18,7 +20,7 @@ const Header = ({ toggle }) => {
       </div>
       <div className="flex-row header-right">
         <AiOutlineSearch className="icon-active header-icon" />
-        <MdDarkMode className="header-icon" />
+        <MdDarkMode className="header-icon" onClick={toggleTheme}/>
         <MdLogout className="header-icon" />
       </div>
     </header>
