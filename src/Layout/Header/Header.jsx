a3b92f39e434cbecf60771f6efdc7e33d3a83e27
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-03-31 19:00:32 +0530

    [style] : theme and icon styling added

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "./note-header.css";
 import { AiOutlineSearch, AiOutlineMenu } from "react-icons/ai";
-import { MdDarkMode, MdLogout } from "react-icons/md";
+import { MdDarkMode, MdLogout, MdLightMode } from "react-icons/md";
 import { useTheme } from "../../context/theme-context";
 
 export const Header = ({ toggle }) => {
-  const {toggleTheme} = useTheme();
+  const { theme, toggleTheme } = useTheme();
   return (
     <header className="flex-row header-bar">
       <div className="flex-row spc-btwn header-left">
         <button className="ham-burger" onClick={toggle}>
           <AiOutlineMenu className="header-icon" />
         </button>
-        <h1 className="flex-row web-logo"><i>MON</i>KEEP</h1>
+        <h1 className="flex-row web-logo">
+          <i>MON</i>KEEP
+        </h1>
       </div>
       <div className="flex-row header-mid">
         <AiOutlineSearch className="header-icon" />
@@ -20,10 +22,13 @@ export const Header = ({ toggle }) => {
       </div>
       <div className="flex-row header-right">
         <AiOutlineSearch className="icon-active header-icon" />
-        <MdDarkMode className="header-icon" onClick={toggleTheme}/>
+        {theme ==="light" ? (
+          <MdDarkMode className="header-icon" onClick={toggleTheme} />
+        ) : (
+          <MdLightMode className="header-icon" onClick={toggleTheme} />
+        )}
         <MdLogout className="header-icon" />
       </div>
     </header>
   );
 };
-
