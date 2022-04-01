3dbce4305d35c304d881e734b5ecd0a1115d0a80
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-01 11:14:07 +0530

    [chore] : ```react-masonry-css``` lib added for note layout

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -4,27 +4,38 @@ import "./note.css";
 import { FiTrash2 } from "react-icons/fi";
 import { RiInboxArchiveLine } from "react-icons/ri";
 import { MdOutlineColorLens } from "react-icons/md";
+import Masonry from "react-masonry-css";
+
+//...
 
 export const Note = () => {
   const { note, removeFromnote } = useNotes();
   return (
     <div className="note-collection">
-      {note.map((item) => {
-        return (
-          <div className="note" key={item._id}>
-            <div>{item.NoteContent.title}</div>
-            <div
-              className="note-descr"
-              dangerouslySetInnerHTML={{ __html: item.NoteContent.description }}
-            />
-            <div className="flex-row note-options-set">
-              <MdOutlineColorLens onClick={() => {}}/>
-              <RiInboxArchiveLine onClick={() => {}} />
-              <FiTrash2 onClick={() => removeFromnote(item._id)} />
+      <Masonry
+        breakpointCols={3}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {note.map((item) => {
+          return (
+            <div className="note" key={item._id}>
+              <div>{item.NoteContent.title}</div>
+              <div
+                className="note-descr"
+                dangerouslySetInnerHTML={{
+                  __html: item.NoteContent.description,
+                }}
+              />
+              <div className="flex-row note-options-set">
+                <MdOutlineColorLens onClick={() => {}} />
+                <RiInboxArchiveLine onClick={() => {}} />
+                <FiTrash2 onClick={() => removeFromnote(item._id)} />
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </Masonry>
     </div>
   );
 };
