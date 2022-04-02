37a13703559b37de4bacbe9c45ceea77a1ff06c1
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-03 00:41:23 +0530

    [REFACTOR] : Highly refactored code for multiple ```route``` setup

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -3,16 +3,26 @@ import "./note-header.css";
 import { AiOutlineSearch, AiOutlineMenu } from "react-icons/ai";
 import { MdDarkMode, MdLogout, MdLightMode } from "react-icons/md";
 import { useTheme } from "../../context/theme-context";
+import { useHeader } from "../../context/header-context";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/auth-context";
 
-export const Header = ({ toggle }) => {
+export const Header = () => {
   const { theme, toggleTheme } = useTheme();
+  const { toggle } = useHeader();
+  const navigate = useNavigate();
+  const { user, signOutHandler } = useAuth();
+  const { pathname } = useLocation();
+  const forbiddenpaths = ["/", "/login", "/signup", "/*"];
   return (
     <header className="flex-row header-bar">
       <div className="flex-row header-left">
-        <button className="ham-burger" onClick={toggle}>
-          <AiOutlineMenu className="header-icon" />
-        </button>
-        <h1 className="flex-row web-logo">
+        {!forbiddenpaths.includes(pathname) && (
+          <button className="ham-burger" onClick={toggle}>
+            <AiOutlineMenu className="header-icon" />
+          </button>
+        )}
+        <h1 className="flex-row web-logo" onClick={() => navigate("/")}>
           <i>MON</i>KEEP
         </h1>
       </div>
@@ -20,14 +30,31 @@ export const Header = ({ toggle }) => {
         <AiOutlineSearch className="header-icon" />
         <input type="text" placeholder="Search" className="search-bar" />
       </div>
-      <div className="flex-row header-right">
+      <div
+        className={
+          !user.isUserLoggedIn
+            ? "flex-row header-righthm "
+            : "flex-row header-right"
+        }
+      >
         <AiOutlineSearch className="icon-active header-icon" />
-        {theme ==="light" ? (
+        {theme === "light" ? (
           <MdDarkMode className="header-icon" onClick={toggleTheme} />
         ) : (
           <MdLightMode className="header-icon" onClick={toggleTheme} />
         )}
-        <MdLogout className="header-icon" />
+        {!user.isUserLoggedIn ? (
+          <div className="flex-row header-right-btn">
+            <button className="btn" onClick={() => navigate("/login")}>
+              LOGIN
+            </button>
+            <button className="btn" onClick={() => navigate("/signup")}>
+              SIGNUP
+            </button>
+          </div>
+        ) : (
+          <MdLogout className="header-icon" onClick={() => signOutHandler()} />
+        )}
       </div>
     </header>
   );
