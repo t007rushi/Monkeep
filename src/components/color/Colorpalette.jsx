f3fe6f055288ea9412baaf00b574a5b3b457bb29
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-02 12:34:44 +0530

    [Feat] : ```Color-Palette``` component added

---
+++ src/components/color/Colorpalette.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./color-palette.css";
+import { MdOutlineColorLens } from "react-icons/md";
+
+const colors = [
+  "default-color",
+  "color-1",
+  "color-2",
+  "color-3",
+  "color-4",
+  "color-5",
+  "color-6",
+  "color-7",
+  "color-8",
+  "color-9",
+];
+const Colorpalette = ({ color, updateColor }) => {
+  const [showColors, setShowColors] = useState(false);
+  return (
+    <div>
+      <div className="color-palette-container">
+        <MdOutlineColorLens
+          className="color-palette-icon"
+          onClick={() => setShowColors((prev) => !prev)}
+        />
+
+        {showColors && (
+          <div className="color-wrapper color-palette">
+            {colors.map((current_color) => {
+              return (
+                <div
+                  key={current_color}
+                  className={`colors ${current_color}`}
+                  onClick={() => {
+                    updateColor(current_color);
+                  }}
+                ></div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Colorpalette;
