68daae448e99ac9291cb0201addc6360572194fe
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-02 14:01:23 +0530

    [Feat]: reducer feat with initial state added

--- src/components/CreateNote/CreateNote.jsx
+++ src/components/CreateNote/CreateNote.jsx
@@ -1,81 +1,81 @@
 import React, { useState } from "react";
 import "./create-note.css";
 import Editor from "../Editor/Editor";
-import { MdOutlineColorLens } from "react-icons/md";
+
 import { BsFillPinFill, BsPin } from "react-icons/bs";
 import { useNotes } from "../../context/notes-context";
+import Colorpalette from "../color/Colorpalette";
+import { SET_TITLE, PIN, CLEAR_INPUTS, SET_COLOR } from "../../constants/notesConstant";
 
 export const CreateNote = () => {
   const [expand, setExpand] = useState(false);
+
+  const [description, setDescription] = useState("");
   const closerEditor = () => {
     setExpand(false);
   };
   const expandEditor = () => {
     setExpand(true);
   };
+  const { notesData, noteDispatcher, addNotes, initialState } = useNotes();
 
-  const [notedata, setNotedata] = useState({
-    title: "",
-    description: "",
-    label: "",
-    pin: false,
-  });
-
-  const { addNotes } = useNotes();
   const handleInput = (e) => {
-    setNotedata({ ...notedata, description: e });
+    setDescription(e);
   };
   return (
     <form
-      className="create-note"
+      className={`create-note ${notesData.color}`}
       onSubmit={(e) => {
         e.preventDefault();
         closerEditor();
-        addNotes(notedata);
-        setNotedata({ title: "", description: "", label: "" });
+        addNotes({ ...notesData, description: description, inTrash: false });
+        noteDispatcher({ type: CLEAR_INPUTS, payload: initialState });
+        setDescription("");
       }}
     >
       <div className="flex-row note-title-pin spc-btwn ">
         <input
           className="note-title-input"
-          value={notedata.title}
+          value={notesData.title}
           onClick={expandEditor}
           placeholder={expand ? "Title" : "Take a Note..."}
-          onChange={(e) => setNotedata({ ...notedata, title: e.target.value })}
+          onChange={(e) =>
+            noteDispatcher({ type: SET_TITLE, payload: e.target.value })
+          }
         />
         {expand && (
           <button
             className="note-pin"
             type="button"
-            onClick={(e) => setNotedata({ ...notedata, pin: !notedata.pin })}
+            onClick={() => noteDispatcher({ type: PIN })}
           >
-            {notedata.pin ? <BsFillPinFill /> : <BsPin />}
+            {notesData.ispin ? <BsFillPinFill /> : <BsPin />}
           </button>
         )}
       </div>
       {expand && (
         <div>
-          <Editor handleInput={handleInput} />
+          <Editor handleInput={handleInput} description={description} />
           <nav className="flex-row note-options">
             <input
               type="text"
               placeholder="enter label"
               className="label-txt"
-              onChange={(e) =>
-                setNotedata({ ...notedata, label: e.target.value })
-              }
+              onChange={(e) => {}}
             />
-
-            <MdOutlineColorLens class="color-palette" />
-            <div className="color-palette-container">
-
-            </div>
-            <button type="submit" className="btn primary-btn add-btn">
+            <Colorpalette updateColor={
+              color => noteDispatcher({ type: SET_COLOR, payload: color })
+            } />
+            <button type="submit" className="btn add-btn">
               Add
             </button>
             <button
               className="btn secondary-btn add-btn close-btn"
-              onClick={closerEditor}
+              onClick={() => {
+                closerEditor();
+                noteDispatcher({ type: CLEAR_INPUTS, payload: initialState });
+                setDescription("");
+              }}
             >
               Close
             </button>
