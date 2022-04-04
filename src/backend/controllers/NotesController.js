93fcfccff18c748f03116f3562da5959dc45b5aa
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-04 21:55:10 +0530

    chore : changed schema post req ```(backend)```

--- src/backend/controllers/NotesController.js
+++ src/backend/controllers/NotesController.js
@@ -7,7 +7,6 @@ import { v4 as uuid } from "uuid";
  *  These are Privately accessible routes.
  * */
 
-
 /**
  * This handler handles gets all notes in the db.
  * send GET Request at /api/notes
@@ -46,11 +45,7 @@ export const createNoteHandler = function (schema, request) {
       );
     }
     const { note } = JSON.parse(request.requestBody);
-    if (!note.tags) {
-      user.notes.push({ ...note, _id: uuid(), tags: [] });
-    } else {
-      user.notes.push({ ...note, _id: uuid() });
-    }
+    user.notes.push({ ...note, _id: uuid() });
     this.db.users.update({ _id: user._id }, user);
     return new Response(201, {}, { notes: user.notes });
   } catch (error) {
