10ddd29a5ad93c0739544de7d330e1d0a399ba80
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-06 23:00:39 +0530

    dipatch update in reducer

--- src/reducer/noteDetailReducer.js
+++ src/reducer/noteDetailReducer.js
@@ -4,9 +4,11 @@ import {
   PIN,
   CLEAR_INPUTS,
   ADD_LABEL,
+  SET_PRIORITY,
 } from "../constants/notesConstant";
 
 export const noteDetailReducer = (state, action) => {
+
   switch (action.type) {
     case SET_TITLE:
       return { ...state, title: action.payload };
@@ -26,6 +28,8 @@ export const noteDetailReducer = (state, action) => {
         ...state,
         labels: updatedLabel,
       };
+      case SET_PRIORITY:
+        return{...state, priority: action.payload};
     default:
       return state;
   }
