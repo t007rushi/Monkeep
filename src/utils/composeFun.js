ad87619442fa4da6382bf8ff90941655bc7b73f7
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-07 14:00:02 +0530

    removed comments

--- src/utils/composeFun.js
+++ src/utils/composeFun.js
@@ -4,36 +4,3 @@ export const composeFilterFunctions =
     functions.reduce((acc, curr) => {
       return curr(state, acc);
     }, notes);
-
-//Filter by priority
-// export const filterbyPriority = (state, notes) => {
-//   if (state.priorities.length !== 0) {
-//     return notes.filter((item) =>
-//       state.priorities.includes(item.Note.priority)
-//     );
-//   }
-//   return notes;
-// };
-//Filter by labels
-// export const filterbyLabels = (state, notes) => {
-//   if (state.tags.length !== 0) {
-//     return notes.filter(
-//       (item) =>
-//         item.Note.labels.filter((tag) => state.tags.includes(tag)).length > 0
-//     );
-//   }
-//   return notes;
-// };
-//sort by date
-// export const sortbydate = (state, notes) => {
-//   if (state.sortby === "old") {
-//     return [...notes].sort(
-//       (a, b) => Date.parse(a.Note.createdAt) - Date.parse(b.Note.createdAt)
-//     );
-//   } else if (state.sortby === "new") {
-//     return [...notes].sort(
-//       (a, b) => Date.parse(b.Note.createdAt) - Date.parse(a.Note.createdAt)
-//     );
-//   }
-//   return notes;
-// };
