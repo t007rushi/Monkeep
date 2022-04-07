c2ddc9d0d34b61c24280b3943ea02054c6a16852
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-04-07 19:19:26 +0530

    search bar positioning fixed

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./note-header.css";
 import { AiOutlineSearch, AiOutlineMenu } from "react-icons/ai";
-import { MdDarkMode, MdLogout, MdLightMode} from "react-icons/md";
-import {BiArrowBack} from "react-icons/bi";
-import {IoMdOptions} from "react-icons/io";
+import { MdDarkMode, MdLogout, MdLightMode } from "react-icons/md";
+import { BiArrowBack } from "react-icons/bi";
+import { IoMdOptions } from "react-icons/io";
 import { useTheme } from "../../context/theme-context";
 import { useHeader } from "../../context/header-context";
 import { NavLink, useLocation } from "react-router-dom";
@@ -11,7 +11,8 @@ import { useAuth } from "../../context/auth-context";
 
 export const Header = () => {
   const { theme, toggleTheme } = useTheme();
-  const { toggle, ShowFilters,showSearch,showSearchbar, hideSearchbar } = useHeader();
+  const { toggle, ShowFilters, showSearch, showSearchbar, hideSearchbar } =
+    useHeader();
   const { user, signOutHandler } = useAuth();
   const { pathname } = useLocation();
   const forbiddenpaths = ["/", "/login", "/signup"];
@@ -27,21 +28,19 @@ export const Header = () => {
           <i>MON</i>KEEP
         </NavLink>
       </div>
-      {user.isUserLoggedIn &&  (
-        <div className={ showSearch?"search-active":"search-hide"}>
-        <div className="flex-row header-mid">
-        <NavLink to="/notes"> <BiArrowBack className="header-icon" onClick={() =>{
-            hideSearchbar();
-          }} /></NavLink>
-          <NavLink to="/search"> <input
-            type="text"
-            placeholder="Search"
-            className="search-bar"
-          /></NavLink>
-          <NavLink to="/search"> <IoMdOptions className="filter-icon" onClick={() => {
-            ShowFilters();
-          }}/></NavLink>
-        </div>
+      {user.isUserLoggedIn && (
+        <div className={showSearch ? "search-active" : "search-hide"}>
+          <div className="flex-row header-mid">
+            <NavLink to="/notes">
+              <BiArrowBack className="header-icon" onClick={hideSearchbar} />
+            </NavLink>
+            <NavLink to="/search">
+              <input type="text" placeholder="Search" className="search-bar" />
+            </NavLink>
+            <NavLink to="/search">
+              <IoMdOptions className="filter-icon" onClick={ShowFilters} />
+            </NavLink>
+          </div>
         </div>
       )}
       <div
@@ -51,9 +50,14 @@ export const Header = () => {
             : "flex-row header-right"
         }
       >
-       { !showSearch && user.isUserLoggedIn && <AiOutlineSearch className="icon-active header-icon" onClick={() =>{
-showSearchbar()
-        }}/>}
+        {!showSearch && user.isUserLoggedIn && (
+          <AiOutlineSearch
+            className="icon-active header-icon"
+            onClick={() => {
+              showSearchbar();
+            }}
+          />
+        )}
         {theme === "light" ? (
           <MdDarkMode className="header-icon" onClick={toggleTheme} />
         ) : (
